cb2f611fa14c47949606173e21e504c8a6449fdc
Abdullah-Rehman786 <air711@live.com>
2021-09-17 12:18:05 -0500

    Added Project files

---
+++ DesignStack/DesignStack/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+
+namespace DesignStack
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+
+            var firstStack = new Stack();
+            var trialObjectList = new List<object> {"Object1", "Object2", "Object3", "Object4" };
+            var nullObj = new object();
+
+            //trying to Pop() from an empty Stack
+            try
+            {
+                Console.WriteLine(firstStack.Pop());
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine(e.Message);
+
+            }
+
+            //Adding objects to the stack & catching a null object trying to be added
+            try
+            {
+                firstStack.Push(trialObjectList[0]);
+                firstStack.Push(trialObjectList[1]);
+                firstStack.Push(trialObjectList[2]);
+                firstStack.Push(trialObjectList[3]);
+                firstStack.Push(nullObj);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            //Write order of all objects in the stack
+            firstStack.PublishStack();
+            Console.WriteLine("");
+
+            //Pop each item from the stack
+            foreach (var item in trialObjectList)
+            {
+                Console.WriteLine(firstStack.Pop());
+
+            }
+            
+            firstStack.Clear();
+        }
+    }
+}
---
+++ DesignStack/DesignStack/Stack.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DesignStack
+{
+    public class Stack
+    {
+        private readonly List<object> _stackObjectList = new List<object>();
+
+        public void Push(object obj)
+        {
+            if (obj.ToString() == "System.Object")
+            {
+                throw new InvalidOperationException("You cannot push a null object onto the stack.");
+            }
+            else
+            {
+                _stackObjectList.Add(obj);
+            }
+        }
+
+        public object Pop()
+        {
+            var i = (_stackObjectList.Count - 1);
+            if (i <= -1)
+            {
+                throw new InvalidOperationException("You cannot pop an object from an empty stack.");
+            }
+            else
+            {
+                var poppedObj = _stackObjectList[i];
+                _stackObjectList.RemoveAt(i);
+                return poppedObj;
+            }
+            
+            
+        }
+
+        public void PublishStack()
+        {
+            foreach (var stackobj in _stackObjectList)
+            {
+                Console.WriteLine(stackobj);
+            }
+        }
+        public void Clear()
+        {
+            _stackObjectList.Clear();
+        }
+    }
+}
